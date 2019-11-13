9ee7035efbc61e26ff77101bb1dfb4ddebde30cc
Adrian Campo Calvo <Adri@MacBook-Pro-de-Adrian.local>
2019-11-13 13:29:13 +0100

    update files

--- staff/adri-campo/tasks/tasks-api/logic/register-user/index.js
+++ staff/adri-campo/tasks/tasks-api/logic/register-user/index.js
@@ -1,7 +1,8 @@
 const validate = require('../../utils/validate')
-const users = require('../../data/users.json')
+const users = require('../../data/users')
 const fs = require('fs')
 const path = require('path')
+const uuid = require('uuid/v4')
 
 module.exports = function(name, surname, email, username, password) {
     validate.string(name)
@@ -16,17 +17,14 @@ module.exports = function(name, surname, email, username, password) {
     validate.string.notVoid('password', password)
 
     return new Promise((resolve, reject) => {
+        const user = users.find(user => username === username)
+
+        if(user) return reject (Error(`user with username ${username} already exists`))
+
+        const id = uuid()
         
-        users.forEach(user => {
-            if(user.username === username || user.email === email) return reject("User already exists")
-
-        })
-
-
         users.push({ name, surname, email, username, password })
-
-
            
-        fs.writeFile(path.join(__dirname, '../../data/users.json'), JSON.stringify(users), error => error? reject(error) : resolve())
+        fs.writeFile(path.join(__dirname, '../../data/users'), JSON.stringify(users), error => error? reject(error) : resolve())
     })
 }
