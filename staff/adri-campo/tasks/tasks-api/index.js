c3c036b487868509fb54751f648479accfe3e757
Adrian Campo Calvo <Adri@MacBook-Pro-de-Adrian.local>
2019-11-14 12:58:41 +0100

    update files

--- staff/adri-campo/tasks/tasks-api/index.js
+++ staff/adri-campo/tasks/tasks-api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { name, version } = require('./package.json')
 const users = require('./data/users')()
-const { registerUser, authenticateUser, retrieveUser } = require('./logic')
+const { registerUser, authenticateUser, retrieveUser, retrieveUsers } = require('./logic')
 const { ConflictError, CredentialsError, NotFoundError } = require('./utils/errors')
 
 const api = express()
@@ -11,22 +11,39 @@ const jsonBodyParser = bodyParser.json()
 
 const { argv: [, , port = 8080] } = process
 
-api.post('/users', jsonBodyParser, (req, res) => {
-    const { body: { name, surname, email, username, password } } = req
+api.route('/users')
 
-    try {
-        registerUser(name, surname, email, username, password)
-            .then(() => res.json({ message: 'user registered successfully' }))
+    .get((req,res) => {
+        try {
+            retrieveUsers()
+            .then(users => res.json(users)) // TODO token
             .catch(error => {
-                if (error instanceof ConflictError)
-                    return res.status(409).json({ message: error.message })
+                if (error instanceof CredentialsError)
+                    return res.status(401).json({ message: error.message })
 
                 res.status(500).json({ message: error.message })
             })
-    } catch ({ message }) {
-        res.status(400).json({ message })
-    }
-})
+        } catch ({ message }) {
+            res.status(400).json({ message })
+        }
+    })
+
+    .post(jsonBodyParser, (req, res) => {
+        const { body: { name, surname, email, username, password } } = req
+
+        try {
+            registerUser(name, surname, email, username, password)
+                .then(() => res.json({ message: 'user registered successfully' }))
+                .catch(error => {
+                    if (error instanceof ConflictError)
+                        return res.status(409).json({ message: error.message })
+
+                    res.status(500).json({ message: error.message })
+                })
+        } catch ({ message }) {
+            res.status(400).json({ message })
+        }
+    })
 
 api.post('/auth', jsonBodyParser, (req, res) => {
     const { body: { username, password } } = req
---
+++ staff/adri-campo/tasks/tasks-api/logic/retrieve-users/index.js
@@ -0,0 +1,15 @@
+const validate = require('../../utils/validate')
+const users = require('../../data/users')()
+const { NotFoundError } = require('../../utils/errors')
+
+module.exports = function () {
+
+    return new Promise((resolve, reject) => { debugger
+        const user = users.data
+
+        users.persist().then(() => {
+        
+            resolve(user)
+        })
+    })
+} 
